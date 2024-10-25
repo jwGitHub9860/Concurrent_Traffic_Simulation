ffeff395f7bc8eb3f22b1d5c81ab90018fd45bcf
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2024-10-25 19:53:57 -0400

    Code for Task FP.4a: deleted "sleep_for()" code

--- CppND-Program-a-Concurrent-Traffic-Simulation-master/src/TrafficLight.cpp
+++ CppND-Program-a-Concurrent-Traffic-Simulation-master/src/TrafficLight.cpp
@@ -30,7 +30,6 @@ void MessageQueue<T>::send(T &&msg)
 {
     // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
     // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
-    this_thread::sleep_for(chrono::milliseconds(100));  // mimics work
     lock_guard<mutex> lck_gurd(mtx);    // Locks mutex when "lock_guard" is created & Unlocked when destroyed (goes out of scope)
     _messages.push_back(move(msg));     // adds vector (deque) to queue
     _condition.notify_one();    // unblocks 1 of the threads waiting for condition_variable (_condition)         adds new message to queue & sends notification afterwards
