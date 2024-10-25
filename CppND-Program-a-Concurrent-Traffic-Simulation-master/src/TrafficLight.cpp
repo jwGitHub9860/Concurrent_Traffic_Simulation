72c354f11825bd64aa8ddff236dc830a21c3230b
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2024-10-24 21:29:17 -0400

    Code for Task FP.2a: made boolean for "red" & "green" traffic lights

--- CppND-Program-a-Concurrent-Traffic-Simulation-master/src/TrafficLight.cpp
+++ CppND-Program-a-Concurrent-Traffic-Simulation-master/src/TrafficLight.cpp
@@ -73,10 +73,10 @@ void TrafficLight::cycleThroughPhases()
         {
             auto cycle_duration = chrono::seconds((rand() % 3) + 4);    // limits cycle duration to 4-6 seconds       (rand() % 3) ---> produces random number between 0 & 2
 
-            _currentPhase = TrafficLightPhase::red;
-            cout << "Traffic Light is red." << endl;    // Traffic Light message
+            _currentPhase = (_currentPhase == TrafficLightPhase::red) ? TrafficLightPhase::red : TrafficLightPhase::green;  // toggles between "red" & "green"
+            cout << "Traffic Light is " << _currentPhase << endl;    // Traffic Light message
             //this_thread::sleep_for(chrono::seconds(rand_value));    // waits between 4 to 6 seconds before switching light
-            _currentPhase = TrafficLightPhase::green;
+            //_currentPhase = TrafficLightPhase::green;
             cout << "Traffic Light is green." << endl;    // Traffic Light message
             //this_thread::sleep_for(chrono::seconds(rand_value));    // waits between 4 to 6 seconds before switching light
 
