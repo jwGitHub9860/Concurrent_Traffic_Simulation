c6e8111f9f99df368efb5a5ef11fdc8870cf68fb
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2024-10-25 17:10:44 -0400

    Code for Task FP.5b

--- CppND-Program-a-Concurrent-Traffic-Simulation-master/src/TrafficLight.cpp
+++ CppND-Program-a-Concurrent-Traffic-Simulation-master/src/TrafficLight.cpp
@@ -45,13 +45,18 @@ TrafficLight::TrafficLight()    // constructor
 {
     _currentPhase = TrafficLightPhase::red;
 }
-/*
+
 void TrafficLight::waitForGreen()
 {
     // FP.5b : add the implementation of the method waitForGreen, in which an infinite while-loop 
     // runs and repeatedly calls the receive function on the message queue. 
     // Once it receives TrafficLightPhase::green, the method returns.
-}*/
+    while (true)
+    {
+        /* code */
+    }
+    
+}
 
 TrafficLightPhase TrafficLight::getCurrentPhase()
 {
